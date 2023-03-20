e6f7bd932a12501b6a1143fa9c4c28bdaa0a2041
Wojtanowicz <pawel.wojtanowicz@capgemini.com>
2023-03-20 11:44:21 +0100

    enum

--- src/o_o_p_Enums/Main.java
+++ src/o_o_p_Enums/Main.java
@@ -1,25 +1,36 @@
-package o_o_p_rectangle;
+package o_o_p_Enums;
+
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Rectangle rec1 = new Rectangle();
-        rec1.setHeight(3);
-        rec1.setWidth(2);                       //this is right to refers to method in other class
-
-
-        Rectangle rec2 = new Rectangle();
-        rec1.setHeight(3);
-        rec2.setWidth(4);
-
-        System.out.println("the number is: " + rec1.getNumber());
-
-        rec1.setNumber(1234);
-        System.out.println("the number is now: " + rec1.getNumber());
-
-
+        whichSeason(Month.FEBRUARY);
+        whichSeason(Month.APRIL);
+        whichSeason(Month.AUGUST);
+        whichSeason(Month.SEPTEMBER);
 
     }
 
+    public static void whichSeason(Month month) {
+
+        switch (month) {
+            case JANUARY, FEBRUARY, DECEMBER:
+                System.out.println("winter season");
+                break;
+            case MARCH, APRIL, MAY:
+                System.out.println("spring season");
+                break;
+            case JUNE, JULY, AUGUST:
+                System.out.println("summer season");
+                break;
+            case SEPTEMBER, OCTOBER, NOVEMBER:
+                System.out.println("fall season");
+                break;
+
+        }
+
+    }
 
 }
--- src/o_o_p_Enums/Month.java
+++ src/o_o_p_Enums/Month.java
@@ -1,2 +1,18 @@
-package o_o_p_Enums;public enum Month {
+package o_o_p_Enums;
+
+// New --> Java Class --> Enum
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+
 }
